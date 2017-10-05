e1bdb29d865ee361812f1a041c0748579c01532b
sergi <sergi@DESKTOP-7M7RV96>
2017-10-05 20:41:10 +0200

    Afegir sergio

---
+++ Sergio.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Sergio
+ *
+ * @author sergi
+ */
+class Sergio {
+    function __construct() {
+        
+    }
+
+    function sumaSuma(){
+        echo "sumasuma";
+    }
+    
+    function restaResta(){
+        echo "restaresta";
+    }
+    
+    function sumaResta(){
+        echo "estaesunbait";
+    }
+}
